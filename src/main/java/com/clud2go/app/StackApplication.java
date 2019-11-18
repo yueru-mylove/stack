1e533e8e0b2597892046f7be27fb1a75cd293ff5
wanjun.wang <wanjun.wang@cloudtogo.cn>
2019-11-18 18:11:43 +0800

    init repositry

---
+++ src/main/java/com/clud2go/app/StackApplication.java
@@ -0,0 +1,22 @@
+package com.clud2go.app;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class StackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StackApplication.class, args);
+    }
+
+
+    @GetMapping("/")
+    public String test() {
+        return "test";
+    }
+}
